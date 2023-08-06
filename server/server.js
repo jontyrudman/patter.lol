b0796387bf4b1198850a7aa0f8a0fe49ca3bf656
Jonathan Rudman <jonathan.rudman@live.co.uk>
2023-08-06 12:26:41 +0100

    Refactor

--- server/server.js
+++ server/server.js
@@ -12,6 +12,73 @@ function uniqueName() {
   return name;
 }
 
+/**
+ * Runs for each new socket.
+ * If the client already has a name, assign it to this socket too.
+ * Assign a new client (based on IP) a random name on the socket.
+ */
+function usernameMiddleware(socket, next) {
+  if (socket.handshake.address in connectedUsers) {
+    socket.username = connectedUsers[socket.handshake.address].username;
+    console.log(
+      "User already found for address %s %s",
+      socket.handshake.address,
+      socket.username
+    );
+    // Count duplicate connections so we know when to clear after all disconnected
+    connectedUsers[socket.handshake.address].sockets.push(socket.id);
+  } else {
+    const username = uniqueName();
+    connectedUsers[socket.handshake.address] = {
+      sockets: [socket.id],
+      username,
+    };
+    socket.username = username;
+    console.log("Assigned new user %s", username);
+  }
+
+  next();
+}
+
+function onConnect(socket) {
+  console.log(
+    "Connection made to socket id %s with user ID %s",
+    socket.id,
+    socket.username
+  );
+
+  // Send a name to the client
+  socket.emit("assign-name", socket.username);
+}
+
+function registerOnDisconnectListener(socket) {
+  // Remove the socket from connectedUsers or remove user if there is only one
+  socket.on("disconnect", () => {
+    console.log(
+      "%s (%s) disconnected",
+      socket.username,
+      socket.handshake.address
+    );
+
+    // When all sockets with a given username are disconnected, remove from connectedUsers
+    if (connectedUsers[socket.handshake.address].sockets.length <= 1) {
+      console.log(
+        "All connections from %s disconnected, removing user %s",
+        socket.handshake.address,
+        socket.username
+      );
+      delete connectedUsers[socket.handshake.address];
+    } else {
+      const i = connectedUsers[socket.handshake.address].sockets.indexOf(
+        socket.id
+      );
+      if (i !== -1) {
+        connectedUsers[socket.handshake.address].sockets.splice(i, 1);
+      }
+    }
+  });
+}
+
 function setup() {
   const io = new Server(5000, {
     cors: {
@@ -19,69 +86,12 @@ function setup() {
     },
   });
 
-  io.use((socket, next) => {
-    /*
-     * Runs for each new socket.
-     * If the client already has a name, assign it to this socket too.
-     * Assign a new client (based on IP) a random name on the socket.
-     */
-    if (socket.handshake.address in connectedUsers) {
-      socket.userID = connectedUsers[socket.handshake.address];
-      console.log(
-        "User already found for address %s %s",
-        socket.handshake.address,
-        socket.userID
-      );
-      // Count duplicate connections so we know when to clear after all disconnected
-      connectedUsers[socket.handshake.address].sockets.push(socket.id);
-    } else {
-      const userID = uniqueName();
-      connectedUsers[socket.handshake.address] = {
-        sockets: [socket.id],
-        userID,
-      };
-      socket.userID = userID;
-      console.log("Assigned new user %s", userID);
-    }
-
-    next();
-  });
+  io.use(usernameMiddleware);
 
   io.on("connection", (socket) => {
-    console.log(
-      "connection made to socket id %s with user ID %s",
-      socket.id,
-      socket.userID
-    );
+    onConnect(socket);
 
-    // Send a name to the client
-    socket.emit("assign-name", socket.userID);
-
-    // Remove the socket from connectedUsers or remove user if there is only one
-    socket.on("disconnect", () => {
-      console.log(
-        "%s (%s) disconnected",
-        socket.userID,
-        socket.handshake.address
-      );
-
-      // When all sockets with a given userID are disconnected, remove from connectedUsers
-      if (connectedUsers[socket.handshake.address].sockets.length <= 1) {
-        console.log(
-          "All connections from %s disconnected, removing user %s",
-          socket.handshake.address,
-          socket.userID
-        );
-        delete connectedUsers[socket.handshake.address];
-      } else {
-        const i = connectedUsers[socket.handshake.address].sockets.indexOf(
-          socket.id
-        );
-        if (i !== -1) {
-          connectedUsers[socket.handshake.address].sockets.splice(i, 1);
-        }
-      }
-    });
+    registerOnDisconnectListener(socket);
   });
 }
 
