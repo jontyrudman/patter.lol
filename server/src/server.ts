70eb1b8886dbf1edeffee30692e89d65d05aabac
Jonathan Rudman <jonathan.rudman@live.co.uk>
2023-08-09 00:10:10 +0100

    Add HTTP server to get TURN creds

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,13 @@
-require('dotenv').config();
+import { createServer } from "http";
+import express from "express";
+import cors from "cors";
+import crypto from "crypto";
 import { Server, Socket } from "socket.io";
 // @ts-ignore
 import Moniker from "moniker";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // TODO: Limit number of duplicate connections
 
@@ -16,8 +22,21 @@ type ConnectedUsers = {
 };
 
 const connectedUsers = <ConnectedUsers>{};
+const expressApp = express();
+const httpServer = createServer(expressApp);
 let getSocketById: ((id: string) => UserSocket) | (() => undefined) = () =>
   undefined;
+
+const env = {
+  FRONTEND_ORIGIN: process.env.FRONTEND_ORIGIN,
+  PORT: parseInt(process.env.PORT ?? "5000"),
+  TURN_DOMAIN: process.env.TURN_DOMAIN,
+  TURN_SECRET: process.env.TURN_SECRET ?? "",
+  // Default to a day
+  TURN_SECRET_LIFETIME_SECONDS: parseInt(
+    process.env.TURN_SECRET_LIFETIME_SECONDS ?? "86400"
+  ),
+};
 
 function uniqueName() {
   let name = Moniker.choose();
@@ -75,7 +94,10 @@ function forwardToRecipient(
   );
 
   if (recipientUsername in connectedUsers) {
-    getSocketById(connectedUsers[recipientUsername])?.emit(event, { senderUsername: initiatorSocket.username, ...data });
+    getSocketById(connectedUsers[recipientUsername])?.emit(event, {
+      senderUsername: initiatorSocket.username,
+      ...data,
+    });
   } else {
     console.log(`${recipientUsername} not found`);
     initiatorSocket.emit("rtc-peer-not-found", recipientUsername);
@@ -112,10 +134,59 @@ function registerRtcHandshakeListener(socket: UserSocket) {
   });
 }
 
-function setup() {
-  const io = new Server(Number.parseInt(process.env.PORT ?? "5000"), {
+/**
+ * Makes an array of ICE servers by generating a timestamped username and
+ * a credential in the form of an HMAC.
+ *
+ * The secret used for the HMAC is shared between the TURN server and this one
+ * and the timestamped username is written into the hash.
+ */
+function getICEServers(name: string): Array<Object> {
+  const unixExpiryTimestamp =
+    parseInt((Date.now() / 1000).toString()) + env.TURN_SECRET_LIFETIME_SECONDS;
+
+  const hmac = crypto.createHmac("sha1", env.TURN_SECRET);
+  const username = [unixExpiryTimestamp, name].join(":");
+
+  hmac.setEncoding('utf-8');
+  hmac.write(username);
+  hmac.end();
+  const credential = hmac.read();
+
+  return [
+    {
+      urls: "stun:" + env.TURN_DOMAIN + ":3478",
+    },
+    {
+      username: username,
+      urls: "turns:" + env.TURN_DOMAIN + ":443",
+      credential: credential,
+    },
+    {
+      username: username,
+      urls: "turn:" + env.TURN_DOMAIN + ":3478",
+      credential: credential,
+    },
+  ];
+}
+
+function setupHTTPServer() {
+  expressApp.use(cors({
+    origin: env.FRONTEND_ORIGIN,
+  }));
+  expressApp.use(express.json());
+  expressApp.post("/get-ice-servers", (req, res) => {
+    console.log(req.body);
+    const iceServers = getICEServers(req.body.username);
+    console.log("Sending ICE servers: %s", JSON.stringify(iceServers));
+    res.json({"iceServers": iceServers});
+  });
+}
+
+function setupWebSockets() {
+  const io = new Server(httpServer, {
     cors: {
-      origin: process.env.FRONTEND_ORIGIN,
+      origin: env.FRONTEND_ORIGIN,
     },
   });
 
@@ -132,4 +203,6 @@ function setup() {
   });
 }
 
-setup();
+setupWebSockets();
+setupHTTPServer();
+httpServer.listen(env.PORT);
