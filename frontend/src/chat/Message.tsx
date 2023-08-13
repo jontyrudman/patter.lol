bf916db5814d9abaee9480f9833b4355df49b37a
Jonathan Rudman <jonathan.rudman@live.co.uk>
2023-08-13 10:51:23 +0100

    Fix styling on mobile with UI elems

--- frontend/src/chat/Message.tsx
+++ frontend/src/chat/Message.tsx
@@ -1,19 +1,29 @@
 import styles from "./Message.module.css";
 
 type MessageProps = {
-  senderUsername: string,
-  message: string,
-  timestamp: number,
-}
+  senderUsername: string;
+  message: string;
+  timestamp: number;
+};
 
-export default function Message({senderUsername, message, timestamp}: MessageProps) {
+export default function Message({
+  senderUsername,
+  message,
+  timestamp,
+}: MessageProps) {
   const datetime = new Date(timestamp as number);
+  const today =
+    new Date(Date.now()).toLocaleDateString() === datetime.toLocaleDateString();
 
   return (
     <div className={styles.Message}>
-      <div><b>{senderUsername}:</b></div>
+      <div>
+        <b>{senderUsername}</b>
+      </div>
       <div>{message}</div>
-      <div style={{ textAlign: "right" }}>{datetime.toLocaleDateString()} {datetime.toLocaleTimeString()}</div>
+      <div style={{ textAlign: "right" }}>
+        {!today && `${datetime.toLocaleDateString()} `}{datetime.toLocaleTimeString()}
+      </div>
     </div>
   );
 }
