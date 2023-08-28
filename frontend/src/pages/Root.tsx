797db0379b39586f990bbf39ce602f2940b91813
Jonathan Rudman <jonathan.rudman@live.co.uk>
2023-08-28 22:35:41 +0100

    Fix linting warning

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { signallingSocket } from "../api";
 import { useChatDispatch, useChatState } from "../context";
 import {
