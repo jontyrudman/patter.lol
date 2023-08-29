5a271ca92b76dc37e749581f74822dbbd1df388a
Jonathan Rudman <jonathan.rudman@live.co.uk>
2023-08-29 21:35:00 +0100

    Add site title to error boundary

--- frontend/src/pages/ErrorBoundary.tsx
+++ frontend/src/pages/ErrorBoundary.tsx
@@ -1,5 +1,6 @@
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 import logger from "../utils/logger";
+import styles from "./Root.module.css";
 
 export default function ErrorBoundary() {
   const error = useRouteError();
@@ -7,6 +8,7 @@ export default function ErrorBoundary() {
 
   return (
     <div>
+      <div className={styles.siteName}>patter.lol</div>
       <b>Oops! An error has occurred.</b>
       {isRouteErrorResponse(error) && (
         <>
