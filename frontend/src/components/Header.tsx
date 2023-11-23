310933385501fdb88988bfd3717056db602be474
Jonathan Rudman <jonathan.rudman@live.co.uk>
2023-11-23 15:26:57 +0000

    Remove unused import

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,6 @@ import { signallingSocket } from "../api";
 import styles from "./Header.module.css";
 import LoadingDots from "./LoadingDots";
 import { useChatDispatch, useChatState } from "../context";
-import Button from "./Button";
 
 export default function Header() {
   const { username } = useChatState();
